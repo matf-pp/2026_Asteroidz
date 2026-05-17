b941c69a157565ce9df51f5aa6aa0e223fe836a7
Ljubomir Banovic <ljuba.banovic@gmail.com>
2026-05-17 16:41:17 +0200

    Add player movement

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,58 @@
 use raylib::prelude::*;
+use raylib::consts::KeyboardKey::*;
+
+struct Player {
+    position: Vector2,
+    boxSize: Vector2,
+    speed: f32,
+}
 
 fn main() {
+    let windowWidth = 640;
+    let windowHeight = 480;
+
     let (mut rl, thread) = raylib::init()
-        .size(640, 480)
+        .size(windowWidth, windowHeight)
         .title("Hello, World!")
         .vsync()
         .build();
 
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
+    let mut player = Player {
+        position: Vector2::new(100.0, 100.0),
+        boxSize: Vector2::new(10.0, 10.0),
+        speed: 200.0,
+    };
 
-        d.clear_background(Color::BLACK);
-        d.draw_text("Hello, World!", 12, 12, 24, Color::WHITE);
+    while !rl.window_should_close() {
+        let dt = rl.get_frame_time();
+        if rl.is_key_down(KEY_RIGHT) {
+            player.position.x += player.speed * dt;
+        } 
+        if rl.is_key_down(KEY_LEFT) {
+            player.position.x -= player.speed * dt;
+        } 
+        if rl.is_key_down(KEY_UP) {
+            player.position.y -= player.speed * dt;
+        }
+        if rl.is_key_down(KEY_DOWN) {
+            player.position.y += player.speed * dt;
+        }
+
+        if player.position.x < -player.boxSize.x {
+            player.position.x = windowWidth as f32;
+        }
+        if player.position.x > (windowWidth as f32) + player.boxSize.x {
+            player.position.x = 0.0;
+        }
+        if player.position.y < -player.boxSize.y {
+            player.position.y = windowHeight as f32;
+        }
+        if player.position.y > (windowHeight as f32) + player.boxSize.y {
+            player.position.y = 0.0;
+        }
+
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::WHITE);
+        d.draw_rectangle_v(player.position, player.boxSize, Color::RED);
     }
 }
