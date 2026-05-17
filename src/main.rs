d7bf65cc7238db903100bc1944a65825d7d444c1
Ljubomir Banovic <ljuba.banovic@gmail.com>
2026-05-17 17:01:57 +0200

    Refactor Player struct

--- src/main.rs
+++ src/main.rs
@@ -3,56 +3,62 @@ use raylib::consts::KeyboardKey::*;
 
 struct Player {
     position: Vector2,
-    boxSize: Vector2,
+    box_size: Vector2,
     speed: f32,
 }
 
+impl Player {
+    fn update(&mut self, rl: &RaylibHandle, window_width: i32, window_height: i32) {
+        let dt = rl.get_frame_time();
+        if rl.is_key_down(KEY_RIGHT) {
+            self.position.x += self.speed * dt;
+        } 
+        if rl.is_key_down(KEY_LEFT) {
+            self.position.x -= self.speed * dt;
+        } 
+        if rl.is_key_down(KEY_UP) {
+            self.position.y -= self.speed * dt;
+        }
+        if rl.is_key_down(KEY_DOWN) {
+            self.position.y += self.speed * dt;
+        }
+
+        if self.position.x < -self.box_size.x {
+            self.position.x = window_width as f32;
+        }
+        if self.position.x > (window_width as f32) + self.box_size.x {
+            self.position.x = 0.0;
+        }
+        if self.position.y < -self.box_size.y {
+            self.position.y = window_height as f32;
+        }
+        if self.position.y > (window_height as f32) + self.box_size.y {
+            self.position.y = 0.0;
+        }
+    }
+}
+
 fn main() {
-    let windowWidth = 640;
-    let windowHeight = 480;
+    let window_width = 640;
+    let window_height = 480;
 
     let (mut rl, thread) = raylib::init()
-        .size(windowWidth, windowHeight)
+        .size(window_width, window_height)
         .title("Hello, World!")
         .vsync()
         .build();
 
     let mut player = Player {
         position: Vector2::new(100.0, 100.0),
-        boxSize: Vector2::new(10.0, 10.0),
+        box_size: Vector2::new(10.0, 10.0),
         speed: 200.0,
     };
 
     while !rl.window_should_close() {
-        let dt = rl.get_frame_time();
-        if rl.is_key_down(KEY_RIGHT) {
-            player.position.x += player.speed * dt;
-        } 
-        if rl.is_key_down(KEY_LEFT) {
-            player.position.x -= player.speed * dt;
-        } 
-        if rl.is_key_down(KEY_UP) {
-            player.position.y -= player.speed * dt;
-        }
-        if rl.is_key_down(KEY_DOWN) {
-            player.position.y += player.speed * dt;
-        }
-
-        if player.position.x < -player.boxSize.x {
-            player.position.x = windowWidth as f32;
-        }
-        if player.position.x > (windowWidth as f32) + player.boxSize.x {
-            player.position.x = 0.0;
-        }
-        if player.position.y < -player.boxSize.y {
-            player.position.y = windowHeight as f32;
-        }
-        if player.position.y > (windowHeight as f32) + player.boxSize.y {
-            player.position.y = 0.0;
-        }
-
+        player.update(&rl, window_width, window_height);
         let mut d = rl.begin_drawing(&thread);
+
         d.clear_background(Color::WHITE);
-        d.draw_rectangle_v(player.position, player.boxSize, Color::RED);
+        d.draw_rectangle_v(player.position, player.box_size, Color::RED);
     }
 }
