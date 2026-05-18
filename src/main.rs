eec99148a23d2db13b268b7a89c970c1f039631c
Ljubomir Banovic <ljuba.banovic@gmail.com>
2026-05-18 17:46:44 +0200

    Implement spaceship movement

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
 use raylib::prelude::*;
 use raylib::consts::KeyboardKey::*;
 
+
 struct Player {
     position: Vector2,
+    velocity: Vector2,
     box_size: Vector2,
-    speed: f32,
+    angle: f32,
 }
 
 impl Player {
+    const SPEED: f32 = 1.00;
+    const ROTATION_SPEED : f32 = 2.5;
+
     fn update(&mut self, rl: &RaylibHandle, window_width: i32, window_height: i32) {
         let dt = rl.get_frame_time();
         if rl.is_key_down(KEY_RIGHT) {
-            self.position.x += self.speed * dt;
+            self.angle += Self::ROTATION_SPEED * dt;
         } 
         if rl.is_key_down(KEY_LEFT) {
-            self.position.x -= self.speed * dt;
+            self.angle -= Self::ROTATION_SPEED * dt;
         } 
         if rl.is_key_down(KEY_UP) {
-            self.position.y -= self.speed * dt;
+            self.velocity.x += Self::SPEED * self.angle.sin();
+            self.velocity.y -= Self::SPEED * self.angle.cos();
         }
-        if rl.is_key_down(KEY_DOWN) {
-            self.position.y += self.speed * dt;
-        }
+
+        self.velocity *= 0.99;
+        self.position += self.velocity * dt;
 
         if self.position.x < -self.box_size.x {
             self.position.x = window_width as f32;
@@ -50,8 +56,9 @@ fn main() {
 
     let mut player = Player {
         position: Vector2::new(100.0, 100.0),
+        velocity: Vector2::new(0.0, 0.0),
         box_size: Vector2::new(32.0, 32.0),
-        speed: 200.0,
+        angle: 0.0,
     };
 
     let texture = rl.load_texture(&thread, "assets/spaceship.png").unwrap();
@@ -61,6 +68,14 @@ fn main() {
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::WHITE);
-        d.draw_texture(&texture, player.position.x as i32, player.position.y as i32, Color::WHITE);
+        // d.draw_texture(&texture, player.position.x as i32, player.position.y as i32, Color::WHITE);
+        d.draw_texture_pro(
+            &texture,
+            Rectangle::new(0.0, 0.0, texture.width as f32, texture.height as f32),
+            Rectangle::new(player.position.x, player.position.y, player.box_size.x, player.box_size.y),
+            Vector2::new(player.box_size.x / 2.0, player.box_size.y / 2.0),
+            player.angle.to_degrees(),
+            Color::WHITE
+        )
     }
 }
